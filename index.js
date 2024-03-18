03b9ea401f0ebfe7b248e4db8f1b40cd935e5181
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-03-18 13:10:36 -0500

    filter.md

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(drivers, searchString) {
+    return drivers.filter(driver => driver.toLowerCase() === searchString.toLowerCase());
+}
+function fuzzyMatch(drivers, startingLetters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(startingLetters.toLowerCase()));
+}
+function matchName(drivers, searchString) {
+    return drivers.filter(driver => driver.name === searchString);
+}
